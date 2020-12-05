ef24933e8a267d08345a0fc05ab0ae4bfd866e16
crazylinuxer <kvirikroma@gmail.com>
2020-12-05 19:26:17 +0200

    fixed cors headers

--- frontend_bindings/errors.py
+++ frontend_bindings/errors.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 
+from apis import cors_headers
+
 
 def custom_abort_page(message: str, code: int):
     return render_template("error_page.html", code=str(code), text=message)
@@ -8,8 +10,8 @@ def custom_abort_page(message: str, code: int):
 def bind_error_pages(app: Flask):
     def error_func(err):
         if request.path.startswith("/api/v"):
-            return jsonify(message=err.description), err.code
+            return jsonify(message=err.description), err.code, cors_headers
         return custom_abort_page(err.description, err.code), err.code
 
-    for code in (500, 404, 400, 424):
+    for code in (500, 404, 400, 424, 409, 422, 403, 401):
         app.errorhandler(code)(error_func)
